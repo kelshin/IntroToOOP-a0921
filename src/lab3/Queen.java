f6bb91ba5694a206eddc7cee281c8bea6a47a3f2
Derrick Park <derrick8689@gmail.com>
2021-10-27 13:47:55 -0700

    Add lab3

---
+++ src/lab3/Queen.java
@@ -0,0 +1,17 @@
+package lab3;
+
+public class Queen extends Piece {
+    public Queen(boolean isWhite) {
+        super(9, isWhite);
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Like bishop and rook");
+    }
+
+    @Override
+    public String toString() {
+        return "Queen{value='" + getValue() + "'}";
+    }
+}
